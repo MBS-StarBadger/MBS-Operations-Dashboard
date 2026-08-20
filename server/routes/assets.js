e4dc80cb9b381df49ec08d62252625bbdf1d0d0f
Roy Cooper <rcooper@millerbrossolar.com>
2026-08-20 15:13:23 +0000

    feat: add operations dashboard landing page

--- server/routes/assets.js
+++ server/routes/assets.js
@@ -1,10 +1,6 @@
 const express = require('express');
 
-const pool = require('../db/pool');
-
 const auth = require('../middleware/auth');
-const assetRepository = require('../repositories/assetRepository');
-const activityRepository = require('../repositories/activityRepository');
 const assetController = require('../controllers/assetController');
 
 const router = express.Router();
@@ -17,11 +13,6 @@ router.post('/', auth, assetController.createAsset);
 
 router.put('/:id', auth, assetController.updateAsset);
 
-router.delete('/:id', auth, async (req, res) => {
-  await assetRepository.deleteById(req.params.id);
-  res.json({ success: true });
-});
+router.delete('/:id', auth, assetController.deleteAsset);
 
 module.exports = router;
-
-
